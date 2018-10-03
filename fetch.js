95237a497083cd6820c3fb74d551faf6247d76a0
Ben Scattergood <benscattergood@gmail.com>
2018-10-03 09:45:00 +0100

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,20 @@
+function 
+function test(){ 
+	
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: $('#time-input').val(),
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+	.then(function(json) {
+		$('#start').text('Stop');
+		$('#badge').text(json.body);
+	});
+};
